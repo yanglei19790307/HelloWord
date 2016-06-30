a2dd65be4a59c7450d5ef55ab01331293f3d182e
杨磊 <god_xp@163.com>
2016-06-30 22:28:57 +0800

    Initial Commit

---
+++ HelloWord/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWord
+//
+//  Created by 杨磊 on 16/6/30.
+//  Copyright © 2016年 ruby.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
